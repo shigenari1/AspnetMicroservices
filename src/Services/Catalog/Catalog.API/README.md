8bf0c19f0e38684cd200303a9709cd5f48612561
ssnari <sigelevel2m@gmail.com>
2023-05-28 00:46:07 +0900

    wip: create docker container

---
+++ src/Services/Catalog/Catalog.API/README.md
@@ -0,0 +1,47 @@
+﻿# 商品APIの利用
+---
+## usecase
+これは商品情報を管理するためのAPIです。以下の表は、それぞれのユースケースに対応するメソッドとURIを示しています。
+
+| Method | URI                | ユースケース                                |
+|--------|--------------------|-----------------------------------------|
+| GET    | /api/v1/Catalog     | 製品の一覧を取得                         |
+| GET    | /api/v1/Catalog/{id}| 特定の製品の詳細を取得                   |
+| POST   | /api/v1/Catalog     | 新たな製品をカタログに追加              |
+| PUT    | /api/v1/Catalog/{id}| 特定の製品情報の更新                   |
+| DELETE | /api/v1/Catalog/{id}| 特定の製品をカタログから削除            |
+| GET    | /api/v1/Catalog/search| キーワードによる製品検索             |
+| GET    | /api/v1/Catalog/category/{category}| 特定のカテゴリの製品一覧を取得|
+| POST   | /api/v1/Catalog/{id}/review | 特定の製品にレビューを追加      |
+| GET    | /api/v1/Catalog/{id}/review | 特定の製品のレビュー一覧を取得|
+
+これらのAPIにアクセスするためには適切な認証トークンが必要です。各APIとそのパラメータについての詳細は、詳細なAPIドキュメンテーションをご参照ください。
+
+---
+
+## Archictecture
+-  N層Arcを採用
+	- Data Access Layer
+		- databaseの操作のみを実行します
+	- Business Logic Layer
+		- ビジネスロジックを実行します
+	- Presentation Layer
+		- ユーザとの対話を実行します
+
+![arc](https://user-images.githubusercontent.com/16585369/241391670-70e574f9-4554-44a2-a4de-26a46227bb32.png)
+
+- Repository Patternを採用
+	- 目的
+		- データ層の抽象化
+		- ドメインオブジェクトの処理を一元化
+	- 利点
+		- データアクセスコードがアプリケーションから分離出来ることで変更が必要な箇所が1箇所でOK
+		- コントローラのテストが容易になる
+
+## Setting
+
+### MongoDB
+```
+docker pull mongo
+docker run -d -p 27017:27017 --name shopping-mongo
+```
---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        /// <summary>
+        /// 全ての商品を取得する
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<Product>> GetProducts();
+        
+        /// <summary>
+        /// 製品IDで商品を取得する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Product> GetProduct(string id);
+        
+        /// <summary>
+        /// 製品名で商品を取得する
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        Task<IEnumerable<Product>> GetProductByName(string name);
+
+        /// <summary>
+        /// カテゴリ名で商品を取得する
+        /// </summary>
+        /// <param name="categoryName"></param>
+        /// <returns></returns>
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+
+        /// <summary>
+        /// 商品を作成する
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        Task CreateProduct(Product product);
+        
+        /// <summary>
+        /// 商品を更新する
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        Task<bool> UpdateProduct(Product product);
+        
+        /// <summary>
+        /// 商品を削除する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<bool> DeleteProduct(string id);
+
+
+    }
+}
