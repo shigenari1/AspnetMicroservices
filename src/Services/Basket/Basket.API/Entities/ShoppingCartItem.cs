aed79ef633c62d78eebe78ddd3a03d17d1e2e35e
ssnari <sigelevel2m@gmail.com>
2023-06-19 23:54:25 +0900

    basket microservices developed

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        // 数量
+        public int Quantity { get; set; }
+        
+        // 色
+        public string Color { get; set; }
+        
+        // 値段
+        public decimal Price { get; set; }
+        
+        // 商品ID
+        public string ProductId { get; set; }
+        
+        // 商品名
+        public string ProductName { get; set; }
+    }
+}
